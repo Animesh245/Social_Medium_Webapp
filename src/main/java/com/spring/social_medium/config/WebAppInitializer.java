8f9ba27af4cb51a2e70ad73aab268f83401aeb00
Animesh245 <bdanimesh245@gmail.com>
2022-08-05 23:59:38 +0600

    WebAppInitializer

--- src/main/java/com/spring/social_medium/config/WebAppInitializer.java
+++ src/main/java/com/spring/social_medium/config/WebAppInitializer.java
@@ -1,4 +1,20 @@
 package com.spring.social_medium.config;
 
-public class WebAppInitializer {
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{RootConfig.class, DbConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{ServletConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
 }
