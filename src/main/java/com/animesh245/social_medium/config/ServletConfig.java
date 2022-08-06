c0b63448ccd3adb1a66d8640f4bd35ebca004183
Animesh245 <bdanimesh245@gmail.com>
2022-08-07 03:00:38 +0600

    ServletConfig

---
+++ src/main/java/com/animesh245/social_medium/config/ServletConfig.java
@@ -0,0 +1,21 @@
+package com.animesh245.social_medium.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@EnableWebMvc
+@ComponentScan(basePackages = {"com.animesh245.social_medium.controller"})
+public class ServletConfig implements WebMvcConfigurer {
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        registry.jsp("/WEB-INF/views/",".jsp");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/css/**").addResourceLocations("/WEB-INF/resources/css/");
+    }
+}
