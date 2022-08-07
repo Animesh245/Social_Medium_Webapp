9d6d1b491a3c3aa5421e7b0d53d5374805a30054
Animesh245 <bdanimesh245@gmail.com>
2022-08-08 00:53:14 +0600

    Enums

---
+++ src/main/java/com/animesh245/social_medium/enums/Privacy.java
@@ -0,0 +1,6 @@
+package com.animesh245.social_medium.enums;
+
+public enum Privacy
+{
+    PRIVACY_PUBLIC, PRIVACY_PRIVATE
+}
---
+++ src/main/java/com/animesh245/social_medium/enums/Role.java
@@ -0,0 +1,6 @@
+package com.animesh245.social_medium.enums;
+
+public enum Role
+{
+    ROLE_USER, ROLE_ADMIN
+}
