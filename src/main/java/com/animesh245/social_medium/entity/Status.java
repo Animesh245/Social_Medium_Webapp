1708472753678ca1a30c014dcef31b3db6e2e656
Animesh245 <bdanimesh245@gmail.com>
2022-08-08 00:51:33 +0600

    Status Entity cls

--- src/main/java/com/animesh245/social_medium/entity/Status.java
+++ src/main/java/com/animesh245/social_medium/entity/Status.java
@@ -1,4 +1,55 @@
 package com.animesh245.social_medium.entity;
 
+import com.animesh245.social_medium.enums.Privacy;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "status_tbl")
 public class Status {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "status_text")
+    private String status_text;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "created_by", referencedColumnName = "id")
+    private User user;
+
+    @Column(name = "created_date")
+    private Date created_date;
+
+    @JoinColumn(name = "location_id", referencedColumnName = "id")
+    @OneToOne(fetch = FetchType.LAZY)
+    private Location location;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "privacy")
+    private Privacy privacy;
+
+    @Column(name = "is_deleted")
+    private Boolean is_deleted;
+
+    @OneToMany(fetch = FetchType.LAZY, orphanRemoval = true)
+    @JoinTable(name = "status_attachments", joinColumns = {@JoinColumn(name = "status_id",referencedColumnName = "id")},
+    inverseJoinColumns = {@JoinColumn(name = "attachment_id", referencedColumnName = "id")})
+    private List<Attachment> attachmentList;
+
+    @OneToMany(cascade = CascadeType.ALL,fetch = FetchType.LAZY, orphanRemoval = true)
+    @JoinTable(name = "post_likes", joinColumns = {@JoinColumn(name = "status_id",referencedColumnName = "id")},inverseJoinColumns = {@JoinColumn(name = "user_id", referencedColumnName = "id")})
+    private List<User> liked_by;
 }
