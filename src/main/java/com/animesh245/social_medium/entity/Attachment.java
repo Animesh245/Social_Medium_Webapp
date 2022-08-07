aadfc4d9883f1bf901612f48d6621a52b1b01db7
Animesh245 <bdanimesh245@gmail.com>
2022-08-08 00:52:05 +0600

    Attachment Entity cls

--- src/main/java/com/animesh245/social_medium/entity/Attachment.java
+++ src/main/java/com/animesh245/social_medium/entity/Attachment.java
@@ -1,4 +1,31 @@
 package com.animesh245.social_medium.entity;
 
-public class Attachment {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "attachment_tbl")
+public class Attachment
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "path")
+    private String attachmentPath;
+
+    @Column(name = "name")
+    private String attachmentName;
+
+    @Column(name = "type")
+    private String attachmentType;
 }
