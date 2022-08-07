0cfaa58264e2d4700caf16232e3bb234230ebfb7
Animesh245 <bdanimesh245@gmail.com>
2022-08-08 00:48:04 +0600

    User Entity cls

--- src/main/java/com/animesh245/social_medium/entity/User.java
+++ src/main/java/com/animesh245/social_medium/entity/User.java
@@ -1,9 +1,57 @@
 package com.animesh245.social_medium.entity;
 
-import lombok.Data;
+import com.animesh245.social_medium.enums.Role;
+import lombok.*;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "user_tbl")
+public class User
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Long id;
 
-public class User {
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String emailId;
+
+    @Column(name = "dob")
+    private Date dateOfBirth;
+
+    @Column(name = "password")
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role")
+    private Role role;
+
+    @JoinColumn(name = "location_id", referencedColumnName = "id")
+    @OneToOne(fetch = FetchType.LAZY)
+    private Location location;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "attachment_id", referencedColumnName = "id")
+    private Attachment attachment;
+
+    @OneToMany(orphanRemoval = true, mappedBy = "user",cascade = CascadeType.ALL)
+    private List<Status> statusList;
+
+    @OneToMany(orphanRemoval = true,mappedBy = "user", cascade = CascadeType.ALL)
+
+    private List<Status> postsLiked;
 }
