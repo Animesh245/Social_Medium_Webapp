fb9133ec1f011821ad161466f8d6b5d31ffe7bba
Animesh245 <bdanimesh245@gmail.com>
2022-08-09 02:24:47 +0600

    LocationDTOs

---
+++ src/main/java/com/animesh245/social_medium/dto/requestDto/ReqLocation.java
@@ -0,0 +1,10 @@
+package com.animesh245.social_medium.dto.requestDto;
+
+import lombok.Data;
+
+@Data
+public class ReqLocation
+{
+    private Long id;
+    private String locationName;
+}
---
+++ src/main/java/com/animesh245/social_medium/dto/responseDto/ResLocation.java
@@ -0,0 +1,9 @@
+package com.animesh245.social_medium.dto.responseDto;
+
+import lombok.Data;
+
+@Data
+public class ResLocation
+{
+    private String locationName;
+}
