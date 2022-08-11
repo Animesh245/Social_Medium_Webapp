e8e60ac4e8f70d53c7065c081d82e03b758aa2ec
Animesh245 <bdanimesh245@gmail.com>
2022-08-12 03:16:41 +0600

    NotFoundException

---
+++ src/main/java/com/animesh245/social_medium/exception/NotFoundException.java
@@ -0,0 +1,9 @@
+package com.animesh245.social_medium.exception;
+
+public class NotFoundException extends RuntimeException
+{
+    public NotFoundException(String s)
+    {
+        super(s);
+    }
+}
