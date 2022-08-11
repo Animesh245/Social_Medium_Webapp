e208dfa48756956388f08e41c4cbe5af37003b4e
Animesh245 <bdanimesh245@gmail.com>
2022-08-12 03:19:00 +0600

    Endpoints updated

--- src/main/java/com/animesh245/social_medium/controller/LocationController.java
+++ src/main/java/com/animesh245/social_medium/controller/LocationController.java
@@ -1,10 +1,8 @@
 package com.animesh245.social_medium.controller;
 
-import com.animesh245.social_medium.dto.requestDto.ReqLocation;
-import com.animesh245.social_medium.dto.responseDto.ResLocation;
-import com.animesh245.social_medium.entity.Location;
+import com.animesh245.social_medium.dto.request.ReqLocationDto;
+import com.animesh245.social_medium.dto.response.ResLocationDto;
 import com.animesh245.social_medium.service.LocationService;
-import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
@@ -12,7 +10,7 @@ import org.springframework.web.servlet.ModelAndView;
 import java.util.List;
 
 @Controller
-@RequestMapping("/location")
+@RequestMapping("/locations")
 public class LocationController
 {
     private final LocationService locationService;
@@ -21,12 +19,12 @@ public class LocationController
         this.locationService = locationService;
     }
 
-    @GetMapping("/list")
+    @GetMapping("/")
     public ModelAndView getLocationList()
     {
         ModelAndView modelAndView = new ModelAndView();
-        List<ResLocation> resLocationList = locationService.getLocations();
-        modelAndView.addObject("resLocationList", resLocationList);
+        List<ResLocationDto> resLocationDtoList = locationService.getLocations();
+        modelAndView.addObject("resLocationDtoList", resLocationDtoList);
         modelAndView.setViewName("admin/location/list");
         return modelAndView;
     }
@@ -34,52 +32,56 @@ public class LocationController
     @GetMapping("/add")
     public ModelAndView addLocation()
     {
-        ReqLocation reqLocation = new ReqLocation();
+        ReqLocationDto reqLocationDto = new ReqLocationDto();
         ModelAndView modelAndView = new ModelAndView();
-        modelAndView.addObject("reqLocation", reqLocation);
+        modelAndView.addObject("reqLocationDto", reqLocationDto);
         modelAndView.setViewName("admin/location/create");
         return modelAndView;
     }
 
-    @PostMapping("/save")
-    public ModelAndView saveLocation(@ModelAttribute("reqLocation") ReqLocation reqLocation)
+    @PostMapping("/")
+    public ModelAndView saveLocation(@ModelAttribute("reqLocation") ReqLocationDto reqLocationDto)
     {
-        locationService.saveLocation(reqLocation);
+        locationService.saveLocation(reqLocationDto);
 
         ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("redirect:/location/list" );
+        modelAndView.setViewName("redirect:/locations/" );
         return modelAndView;
     }
 
-    @GetMapping("/get/{id}")
-    public ModelAndView getSingleLocation(@PathVariable String id) throws Exception {
-        ResLocation resLocation = locationService.getLocation(id);
+    @GetMapping("/{id}")
+    public ModelAndView getSingleLocation(@PathVariable String id) throws Exception
+    {
+        ResLocationDto resLocationDto = locationService.getLocation(id);
 
         ModelAndView modelAndView = new ModelAndView();
-        modelAndView.addObject("resLocation", resLocation);
+        modelAndView.addObject("resLocation", resLocationDto);
         modelAndView.setViewName("admin/location/show");
         return modelAndView;
     }
 
-    @PostMapping("/update")
-    public ModelAndView saveUpdates(@ModelAttribute("resLocation") ResLocation resLocation)
-    {
-        ReqLocation reqLocation = new ReqLocation();
-        BeanUtils.copyProperties(resLocation,reqLocation);
+    //Update not required
 
-        locationService.updateLocation(resLocation.getId().toString(),reqLocation);
-
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("redirect:/location/list");
-        return modelAndView;
-    }
+//    @PostMapping("/edit/{id}")
+//    public ModelAndView saveUpdates(@ModelAttribute("resLocation") ResLocationDto resLocationDto)
+//    {
+//        ReqLocationDto reqLocationDto = new ReqLocationDto();
+//        BeanUtils.copyProperties(resLocationDto, reqLocationDto);
+//
+//        locationService.updateLocation(resLocationDto.getId().toString(), reqLocationDto);
+//
+//        ModelAndView modelAndView = new ModelAndView();
+//        modelAndView.setViewName("redirect:/location/list");
+//        return modelAndView;
+//    }
 
     @GetMapping("/delete/{id}")
-    public ModelAndView deleteLocation(@PathVariable String id) throws Exception {
+    public ModelAndView deleteLocation(@PathVariable String id) throws Exception
+    {
         locationService.deleteLocation(id);
 
         ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("redirect:/location/list");
+        modelAndView.setViewName("redirect:/locations/");
         return modelAndView;
     }
 
