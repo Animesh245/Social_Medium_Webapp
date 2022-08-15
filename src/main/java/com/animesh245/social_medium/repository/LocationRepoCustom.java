756a45d02003a8de2cfd2386fd6d11da4eb0e21b
Animesh245 <bdanimesh245@gmail.com>
2022-08-15 16:33:52 +0600

    Removed Custom Jpa

--- src/main/java/com/animesh245/social_medium/repository/LocationRepoCustom.java
+++
@@ -1,8 +0,0 @@
-package com.animesh245.social_medium.repository;
-
-import com.animesh245.social_medium.entity.Location;
-
-public interface LocationRepoCustom
-{
-    Location findByName(String locationName);
-}
