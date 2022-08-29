4d509ac8b80d1aa7b0cda086655234d41342df88
Animesh245 <bdanimesh245@gmail.com>
2022-08-30 02:19:51 +0600

    extends UserDetailsService

--- src/main/java/com/animesh245/social_medium/service/definition/IUserService.java
+++ src/main/java/com/animesh245/social_medium/service/definition/IUserService.java
@@ -3,17 +3,17 @@ package com.animesh245.social_medium.service.definition;
 import com.animesh245.social_medium.dto.request.ReqUserDto;
 import com.animesh245.social_medium.dto.response.ResUserDto;
 import com.animesh245.social_medium.entity.User;
-
+import org.springframework.security.core.userdetails.UserDetailsService;
 import java.util.List;
 
-public interface IUserService
+public interface IUserService extends UserDetailsService
 {
-    List<ResUserDto> getUsers();
+    List<ResUserDto> getUsers() throws Exception;
 
     void saveUser(ReqUserDto reqUserDto) throws Exception;
 
 
-    ResUserDto getUser(String id);
+    ResUserDto getUser(String id) throws Exception;
 
     void updateUser(String id , ReqUserDto reqUserDto) throws Exception;
 
@@ -23,5 +23,5 @@ public interface IUserService
 
     User dtoToEntity(ReqUserDto reqUserDto) throws Exception;
 
-    ResUserDto entityToDto(User user);
+    ResUserDto entityToDto(User user) throws Exception;
 }
