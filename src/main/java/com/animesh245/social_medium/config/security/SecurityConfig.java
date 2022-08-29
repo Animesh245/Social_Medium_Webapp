7a1e01dc99247083b3c3a897663e7c0eedd6cc04
Animesh245 <bdanimesh245@gmail.com>
2022-08-30 02:29:45 +0600

    UserService & customizing antMatchers

--- src/main/java/com/animesh245/social_medium/config/security/SecurityConfig.java
+++ src/main/java/com/animesh245/social_medium/config/security/SecurityConfig.java
@@ -1,11 +1,12 @@
 package com.animesh245.social_medium.config.security;
 
-import com.animesh245.social_medium.service.implementaion.UsrDetails;
+import com.animesh245.social_medium.service.definition.IUserService;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -18,47 +19,50 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig
 {
     private final PasswordEncoder passwordEncoder;
-    private final UsrDetails usrDetails;
+    private final IUserService iUserService;
     private final AuthSuccessHandler authSuccessHandler;
 
-    public SecurityConfig(PasswordEncoder passwordEncoder, UsrDetails usrDetails, AuthSuccessHandler authSuccessHandler)
+    public SecurityConfig(PasswordEncoder passwordEncoder, IUserService iUserService, AuthSuccessHandler authSuccessHandler)
     {
         this.passwordEncoder = passwordEncoder;
-        this.usrDetails = usrDetails;
+        this.iUserService = iUserService;
         this.authSuccessHandler = authSuccessHandler;
     }
 
     @Autowired
     public void configure(@NotNull AuthenticationManagerBuilder managerBuilder) throws Exception
     {
-        managerBuilder.userDetailsService(usrDetails).passwordEncoder(passwordEncoder);
+        managerBuilder.userDetailsService(iUserService).passwordEncoder(passwordEncoder);
     }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception
     {
-        httpSecurity.authorizeHttpRequests()
-                .antMatchers("/image/**", "/css/**", "/js/**", "/vendor/**").permitAll()
-                .antMatchers("/auth/login/**", "/users/create", "/users/").permitAll()
-                .antMatchers("/locations/**","/statuses/delete/**", "/users/delete/**").hasAnyAuthority("ROLE_ADMIN")
-                .antMatchers("/locations/list").hasAnyRole("USER", "ADMIN")
-                .antMatchers("/statuses/create","/statuses/update/**", "/statuses/delete/**", "/users/update/**").hasAnyAuthority("ROLE_USER")
-                .anyRequest().authenticated()
+
+        httpSecurity.csrf().disable()
+                .authorizeHttpRequests()
+                    .antMatchers("/images/**", "/css/*", "/js/*", "/vendor/*").permitAll()
+                    .antMatchers("/auth/**", "/users/create","/users/").permitAll()
+                    .antMatchers("/locations/").hasAnyRole("USER", "ADMIN")
+                    .antMatchers("/locations/**","/statuses/delete/**", "/users/delete/**").hasRole("ADMIN")
+                    .antMatchers(HttpMethod.POST,"/statuses/","/users/").hasRole("USER")
+                    .antMatchers("/statuses/**","/users/update/**").hasRole("USER")
+                    .anyRequest().authenticated()
                 .and()
-                .formLogin()
-                .loginPage("/auth/login")
-                .loginProcessingUrl("/login-processing")
-                .permitAll()
-                .usernameParameter("email")
-                .passwordParameter("password")
-                .successHandler(authSuccessHandler)
-                .failureUrl("/auth/login?error=true")
+                    .formLogin()
+                        .loginPage("/auth/login")
+                        .loginProcessingUrl("/login-processing")
+                        .permitAll()
+                        .usernameParameter("email")
+                        .passwordParameter("password")
+                        .successHandler(authSuccessHandler)
+                        .failureUrl("/auth/login?error=true")
                 .and()
-                .logout()
-                .logoutUrl("/auth/logout")
-                .logoutSuccessUrl("/auth/login").permitAll()
+                        .logout()
+                        .logoutUrl("/auth/logout")
+                        .logoutSuccessUrl("/auth/login").permitAll()
                 .and()
-                .exceptionHandling().accessDeniedPage("/auth/403");
+                        .exceptionHandling().accessDeniedPage("/auth/403");
 
         return httpSecurity.build();
     }
