3d55df4b940ceb327a27492c9479bc1780b484db
Animesh245 <bdanimesh245@gmail.com>
2022-08-30 02:27:15 +0600

    hibernate lazy initialization exception

--- src/main/java/com/animesh245/social_medium/service/implementaion/StatusService.java
+++ src/main/java/com/animesh245/social_medium/service/implementaion/StatusService.java
@@ -1,18 +1,24 @@
 package com.animesh245.social_medium.service.implementaion;
 
 import com.animesh245.social_medium.dto.request.RequestStatusDto;
+import com.animesh245.social_medium.dto.response.ResponseLocationDto;
 import com.animesh245.social_medium.dto.response.ResponseStatusDto;
 import com.animesh245.social_medium.entity.Attachment;
 import com.animesh245.social_medium.entity.Status;
 import com.animesh245.social_medium.entity.User;
 import com.animesh245.social_medium.enums.Privacy;
 import com.animesh245.social_medium.repository.StatusRepo;
+import com.animesh245.social_medium.repository.UserRepo;
 import com.animesh245.social_medium.service.definition.IAttachmentService;
 import com.animesh245.social_medium.service.definition.ILocationService;
 import com.animesh245.social_medium.service.definition.IStatusService;
 import org.springframework.beans.BeanUtils;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.TypedQuery;
 import javax.transaction.Transactional;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -22,19 +28,22 @@ import java.util.List;
 @Transactional
 public class StatusService implements IStatusService
 {
+    private final EntityManagerFactory emf;
     private final StatusRepo statusRepo;
+    private final UserRepo userRepo;
     private final ILocationService iLocationService;
     private final IAttachmentService iAttachmentService;
 
-    public StatusService(StatusRepo statusRepo, ILocationService iLocationService, IAttachmentService iAttachmentService) {
+    public StatusService(EntityManagerFactory emf, StatusRepo statusRepo, UserRepo userRepo, ILocationService iLocationService, IAttachmentService iAttachmentService) {
+        this.emf = emf;
         this.statusRepo = statusRepo;
+        this.userRepo = userRepo;
         this.iLocationService = iLocationService;
         this.iAttachmentService = iAttachmentService;
     }
 
     @Override
-    public List<ResponseStatusDto> getStatuses()
-    {
+    public List<ResponseStatusDto> getStatuses() throws Exception {
         List<Status> statuses = statusRepo.findAll();
         List<ResponseStatusDto> responseStatusDtos = new ArrayList<>();
 
@@ -54,8 +63,7 @@ public class StatusService implements IStatusService
     }
 
     @Override
-    public ResponseStatusDto getStatus(String id)
-    {
+    public ResponseStatusDto getStatus(String id) throws Exception {
         var status = statusRepo.findById(Long.parseLong(id)).orElseThrow();
 
         return entityToDto(status);
@@ -79,16 +87,20 @@ public class StatusService implements IStatusService
 
     }
 
+
     @Override
     public Status dtoToEntity(RequestStatusDto requestStatusDto) throws Exception
     {
         var location = iLocationService.getLocationByName(requestStatusDto.getLocationName());
         List<Attachment> attachments = iAttachmentService.insertInBulks(requestStatusDto.getFileList());
 
+        var user = userRepo.findByEmailId(SecurityContextHolder.getContext().getAuthentication().getName());
+
         var status = new Status();
         BeanUtils.copyProperties(requestStatusDto,status);
         status.setCreatedDate(LocalDateTime.now());
         status.setLocation(location);
+        status.setUser(user);
         status.setPrivacy(Privacy.valueOf(requestStatusDto.getPrivacy()));
         status.setIsDeleted(false);
         status.setAttachmentList(attachments);
@@ -97,35 +109,60 @@ public class StatusService implements IStatusService
     }
 
     @Override
-    public ResponseStatusDto entityToDto(Status status)
-    {
-        //Fetching list of attachmentPath from status table
-        List<Attachment> attachments =status.getAttachmentList();
-        List<String > attachmentPathList = new ArrayList<>();
+    public ResponseStatusDto entityToDto(Status status) throws Exception {
+        //To deal with lazy initialization exception
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
 
-        for (Attachment attachment: attachments)
-        {
-         String path = attachment.getAttachmentPath();
-         attachmentPathList.add(path);
-        }
+        // want to fetch the attachments of a status
+        TypedQuery<Status> q = em.createQuery("SELECT s FROM Status s LEFT JOIN FETCH s.attachmentList WHERE s.id=: id", Status.class).setParameter("id",status.getId());
+        TypedQuery<Status> q2 = em.createQuery("SELECT s FROM Status s LEFT JOIN FETCH  s.likedBy WHERE s.id=: id", Status.class).setParameter("id",status.getId());
+        List<Status> statusList = q.getResultList();//9
+        List<Status> statusList1 = q2.getResultList();
+
+        em.getTransaction().commit();
+        em.close();
+
         //Fetching list of userNames from status table
-        List<User> users = status.getLikedBy();
         List<String> userNames = new ArrayList<>();
-        for (User user: users)
+        var attachmentPathList = new ArrayList<String>();
+
+
+        var status1 = statusList.get(0);
+
+//        for (Status status1: statusList)
+//        {
+                for (Attachment attachment: status1.getAttachmentList())
+                {
+                    attachmentPathList.add(attachment.getAttachmentPath());
+                }
+//        }
+
+        for (Status status2: statusList1)
         {
-            String name = user.getFullName();
-            userNames.add(name);
+            for (User user: status2.getLikedBy())
+            {
+                String name = user.getFullName();
+                userNames.add(name);
+            }
         }
+        var user = status.getUser();
+        Attachment profileImage = user.getAttachment();
+        String path = iAttachmentService.getAttachment(profileImage.getId()).getAttachmentPath();
+        ResponseLocationDto location = iLocationService.getLocation(status.getLocation().getId().toString());
+
         if(!status.getIsDeleted())
         {
             var responseStatusDto = new ResponseStatusDto();
             BeanUtils.copyProperties(status,responseStatusDto);
             responseStatusDto.setId(status.getId().toString());
-            responseStatusDto.setLocationName(status.getLocation().getLocationName());
+            responseStatusDto.setUserName(user.getFullName());
+            responseStatusDto.setProfileImagePath(path);
+            responseStatusDto.setLocationName(location.getLocationName());
             responseStatusDto.setPrivacy(status.getPrivacy().toString());
             responseStatusDto.setCreatedDate(status.getCreatedDate().toString());
             responseStatusDto.setAttachmentPathList(attachmentPathList);
-//            responseStatusDto.setLikedByUser(userNames);
+            responseStatusDto.setLikedByUser(userNames);
             return responseStatusDto;
         }
         return null;
--- src/main/java/com/animesh245/social_medium/service/implementaion/UserService.java
+++ src/main/java/com/animesh245/social_medium/service/implementaion/UserService.java
@@ -1,9 +1,11 @@
 package com.animesh245.social_medium.service.implementaion;
 
 import com.animesh245.social_medium.config.Properties;
-import com.animesh245.social_medium.dto.request.ReqUserDto;
-import com.animesh245.social_medium.dto.response.ResUserDto;
+import com.animesh245.social_medium.dto.request.RequestUserDto;
+import com.animesh245.social_medium.dto.response.ResponseLocationDto;
+import com.animesh245.social_medium.dto.response.ResponseUserDto;
 import com.animesh245.social_medium.entity.Location;
+import com.animesh245.social_medium.entity.Status;
 import com.animesh245.social_medium.entity.User;
 import com.animesh245.social_medium.enums.AccountStatus;
 import com.animesh245.social_medium.enums.Role;
@@ -11,12 +13,18 @@ import com.animesh245.social_medium.exception.NotFoundException;
 import com.animesh245.social_medium.repository.UserRepo;
 import com.animesh245.social_medium.service.definition.IAttachmentService;
 import com.animesh245.social_medium.service.definition.ILocationService;
+import com.animesh245.social_medium.service.definition.IStatusService;
 import com.animesh245.social_medium.service.definition.IUserService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.BeanUtils;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.TypedQuery;
 import javax.transaction.Transactional;
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -26,26 +34,41 @@ import java.util.List;
 @Transactional
 public class UserService implements IUserService
 {
+    private final EntityManagerFactory emf;
     private final UserRepo userRepo;
+    private final IStatusService istatusService;
     private final ILocationService iLocationService;
     private final IAttachmentService iAttachmentService;
     private final ModelMapper modelMapper;
     private final PasswordEncoder passwordEncoder;
 
-    public UserService(UserRepo userRepo, ILocationService iLocationService, IAttachmentService iAttachmentService, ModelMapper modelMapper, PasswordEncoder passwordEncoder)
+    public UserService(UserRepo userRepo, IStatusService istatusService, ILocationService iLocationService, IAttachmentService iAttachmentService, ModelMapper modelMapper, PasswordEncoder passwordEncoder, EntityManagerFactory emf)
     {
         this.userRepo = userRepo;
+        this.istatusService = istatusService;
         this.iLocationService = iLocationService;
         this.iAttachmentService = iAttachmentService;
         this.modelMapper = modelMapper;
         this.passwordEncoder = passwordEncoder;
+        this.emf = emf;
     }
 
     @Override
-    public List<ResUserDto> getUsers()
+    public UserDetails loadUserByUsername(String emailId) throws UsernameNotFoundException
     {
+        var user = userRepo.findByEmailId(emailId);
+
+        if(user == null)
+        {
+            throw new UsernameNotFoundException(emailId);
+        }
+        return new org.springframework.security.core.userdetails.User(user.getEmailId(), user.getPassword(), user.getAuthorities());
+    }
+
+    @Override
+    public List<ResponseUserDto> getUsers() throws Exception {
         var userList = userRepo.findAll();
-        var resUserDtoList = new ArrayList<ResUserDto>();
+        var resUserDtoList = new ArrayList<ResponseUserDto>();
 
         for (User user: userList)
         {
@@ -56,25 +79,24 @@ public class UserService implements IUserService
     }
 
     @Override
-    public void saveUser(ReqUserDto reqUserDto) throws Exception
+    public void saveUser(RequestUserDto requestUserDto) throws Exception
     {
-        var user = dtoToEntity(reqUserDto);
+        var user = dtoToEntity(requestUserDto);
         userRepo.save(user);
         System.out.println(user);
     }
 
     @Override
-    public ResUserDto getUser(String id)
-    {
+    public ResponseUserDto getUser(String id) throws Exception {
         var user = userRepo.findById(Long.parseLong(id)).orElseThrow(() -> new NotFoundException("User not found"));
         return entityToDto(user);
     }
 
     @Override
-    public void updateUser(String id, ReqUserDto reqUserDto) throws Exception
+    public void updateUser(String id, RequestUserDto requestUserDto) throws Exception
     {
         var user = userRepo.findById(Long.parseLong(id)).orElseThrow(() -> new NotFoundException("User not found"));
-        var user1 = dtoToEntity(reqUserDto);
+        var user1 = dtoToEntity(requestUserDto);
         user1.setId(user.getId());
         if(userRepo.existsById(user.getId()))
         {
@@ -101,39 +123,69 @@ public class UserService implements IUserService
     }
 
     //Dto to Entity Conversion
+
     @Override
-    public User dtoToEntity(ReqUserDto reqUserDto) throws Exception
+    public User dtoToEntity(RequestUserDto requestUserDto) throws Exception
     {
-        LocalDate date = LocalDate.parse(reqUserDto.getDateOfBirth());
-        Location location = iLocationService.getLocationByName(reqUserDto.getLocationName());
-        var attachment = iAttachmentService.saveAttachment(reqUserDto.getProfileImage(), Properties.USER_FOLDER);
+        LocalDate date = LocalDate.parse(requestUserDto.getDateOfBirth());
+        Location location = iLocationService.getLocationByName(requestUserDto.getLocationName());
+        var attachment = iAttachmentService.saveAttachment(requestUserDto.getProfileImage(), Properties.USER_FOLDER);
 
 //        var user = modelMapper.map(reqUserDto,User.class);
         var user = new User();
-        BeanUtils.copyProperties(reqUserDto, user);
+        BeanUtils.copyProperties(requestUserDto, user);
         user.setDateOfBirth(date);
         user.setRole(Role.ROLE_USER);
-        user.setPassword(passwordEncoder.encode(reqUserDto.getPassword()));
+        user.setPassword(passwordEncoder.encode(requestUserDto.getPassword()));
         user.setAccountStatus(AccountStatus.ACTIVE);
         user.setAttachment(attachment);
         user.setLocation(location);
         return user;
     }
 
-    //Entity to Dto Conversion
 
+    //Entity to Dto Conversion
     @Override
-    public ResUserDto entityToDto(User user)
-    {
+    public ResponseUserDto entityToDto(User user) throws Exception {
         if (user.getAccountStatus() == AccountStatus.ACTIVE)
         {
-            var resUserDto = modelMapper.map(user,ResUserDto.class);
+
+            //To deal with lazy initialization exception
+            EntityManager em = emf.createEntityManager();
+            em.getTransaction().begin();
+
+            TypedQuery<User> q = em.createQuery("SELECT u FROM User u LEFT JOIN FETCH u.statusList WHERE u.id=: id", User.class).setParameter("id", user.getId());
+            List<User> userList = q.getResultList();
+
+            em.getTransaction().commit();
+            em.close();
+
+            var responseStatusDtoTextList = new ArrayList<String >();
+            var attachmentPathList = new ArrayList<String>();
+            for (User user1: userList)
+            {
+                for (Status status: user1.getStatusList()) {
+                    var responseStatusDto = istatusService.entityToDto(status);
+                    responseStatusDtoTextList.add(responseStatusDto.getStatusText());
+                    attachmentPathList.addAll(responseStatusDto.getAttachmentPathList());
+                }
+            }
+
+            ResponseLocationDto location = iLocationService.getLocation(user.getLocation().getId().toString());
+            var attachment = iAttachmentService.getAttachment(user.getAttachment().getId());
+            var path = attachment.getAttachmentPath();
+
+//            var resUserDto = modelMapper.map(user,ResUserDto.class);
+            var resUserDto = new ResponseUserDto();
+            BeanUtils.copyProperties(user, resUserDto);
             resUserDto.setId(user.getId().toString());
             resUserDto.setDateOfBirth(user.getDateOfBirth().toString());
             resUserDto.setRole(user.getRole().toString());
             resUserDto.setPassword(user.getPassword());
-            resUserDto.setLocationName(user.getLocation().getLocationName());
-            resUserDto.setProfileImagePath(user.getAttachment().getAttachmentPath());
+            resUserDto.setLocationName(location.getLocationName());
+            resUserDto.setStatusDtoTextList(responseStatusDtoTextList);
+            resUserDto.setStatusDtoImagePathList(attachmentPathList);
+            resUserDto.setProfileImagePath(path);
             return resUserDto;
         }
         return null;
